c4fb4d976d178241e619545d2d1da3ffdb0dd4de
Colin Lewis <hello@colinlewis.se>
2021-07-13 04:18:48 +0100

    timer on level 2 submenu

--- js/nav.js
+++ js/nav.js
@@ -1,23 +1,40 @@
 // Add .js class to html tag to confirm JavaScript is enabled
 var html = document.documentElement;
-html.className += ' js';
+html.className += " js";
 
-// Add a one second delay to submenu display for mouse users.
+// Add a one second delay to submenu display for mouse users
 const hasSubmenu = document.querySelectorAll(
   "li.main-nav__menu-item--has-submenu"
 );
 var timerSubmenu;
-Array.prototype.forEach.call(hasSubmenu, function (el, i) {
-  // Add .focus class on mouseover
-  el.addEventListener("mouseover", function (event) {
+Array.prototype.forEach.call(hasSubmenu, function (el) {
+  el.addEventListener("mouseover", function () {
     document
       .querySelector("li.main-nav__menu-item--has-submenu")
       .classList.add("focus");
     clearTimeout(timerSubmenu);
   });
-  // Set the delay on mouseout and remove the .focus class
-  el.addEventListener("mouseout", function (event) {
-    timerSubmenu = setTimeout(function (event) {
+  el.addEventListener("mouseout", function () {
+    timerSubmenu = setTimeout(function () {
+      document.querySelector(".focus").classList.remove("focus");
+    }, 1000);
+  });
+});
+
+// Add a one second delay to sub-submenu display for mouse users
+const hasSubSubmenu = document.querySelectorAll(
+  "li.main-nav__submenu-item--has-sub-submenu"
+);
+var timerSubSubmenu;
+Array.prototype.forEach.call(hasSubSubmenu, function (el) {
+  el.addEventListener("mouseover", function () {
+    document
+      .querySelector("li.main-nav__submenu-item--has-sub-submenu")
+      .classList.add("focus");
+    clearTimeout(timerSubSubmenu);
+  });
+  el.addEventListener("mouseout", function () {
+    timerSubSubmenu = setTimeout(function () {
       document.querySelector(".focus").classList.remove("focus");
     }, 1000);
   });
@@ -27,7 +44,7 @@ Array.prototype.forEach.call(hasSubmenu, function (el, i) {
 var submenuItems = document.querySelectorAll(
   "li.main-nav__menu-item--has-submenu"
 );
-Array.prototype.forEach.call(submenuItems, function (el, i) {
+Array.prototype.forEach.call(submenuItems, function (el) {
   el.querySelector("a").addEventListener("click", function (event) {
     if (this.parentNode.className == "main-nav__menu-item--has-submenu") {
       this.parentNode.className = "main-nav__menu-item--has-submenu open";
@@ -42,25 +59,23 @@ Array.prototype.forEach.call(submenuItems, function (el, i) {
 });
 
 // Operate the nav using focus and blur – i.e. tabbing via keyboard
-// Get links with the submenu and sub-submenu classes
 const submenuLinks = document.querySelectorAll(
   ".main-nav__menu-item-link, .main-nav__submenu-item-link"
 );
 submenuLinks.forEach((link) => {
   if (link.nextElementSibling) {
     link.addEventListener("focus", function () {
-      // Add a focus event listener to the link
-      this.parentElement.classList.add("focus"); // Add the .focus class to the parent li
-      this.setAttribute("aria-expanded", "true"); // Set aria-expanded to true on the child link
+      this.parentElement.classList.add("focus");
+      this.setAttribute("aria-expanded", "true");
     });
-    const subMenu = link.nextElementSibling; // Check for a sibling item
-    const subMenuLinks = subMenu.querySelectorAll("a"); // Get the link(s)
+    const subMenu = link.nextElementSibling;
+    const subMenuLinks = subMenu.querySelectorAll("a");
     const lastLinkIndex = subMenuLinks.length - 1;
     const lastLink = subMenuLinks[lastLinkIndex];
     lastLink.addEventListener("blur", function () {
       // Blur event
-      link.parentElement.classList.remove("focus"); // Remove the .focus class
-      link.setAttribute("aria-expanded", "false"); // Set aria-expanded to false
+      link.parentElement.classList.remove("focus");
+      link.setAttribute("aria-expanded", "false");
     });
   }
 });
@@ -69,11 +84,11 @@ submenuLinks.forEach((link) => {
 var toggle = document.querySelector("#nav-toggle");
 var menu = document.querySelector("#menu");
 toggle.addEventListener("click", function () {
-  if (menu.classList.contains("is-active")) {
-    menu.classList.remove("is-active");
-    toggle.classList.remove("is-active");
+  if (menu.classList.contains("js-is-active")) {
+    menu.classList.remove("js-is-active");
+    toggle.classList.remove("js-is-active");
   } else {
-    menu.classList.add("is-active");
-    toggle.classList.add("is-active");
+    menu.classList.add("js-is-active");
+    toggle.classList.add("js-is-active");
   }
 });
